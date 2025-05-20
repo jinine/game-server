1fb681a3aa94f64bf3b95ec7812347e893237fe8
tristan engen <70934782+jinine@users.noreply.github.com>
2025-05-19 22:23:53 -0400

    Update auth.py

--- auth.py
+++ auth.py
@@ -5,10 +5,12 @@ from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from models import Player
+from dotenv import load_dotenv
+import os
 
-# to get a string like this run:
-# openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+load_dotenv()
+SECRET_KEY=os.getenv('SECRET_KEY')
+
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
